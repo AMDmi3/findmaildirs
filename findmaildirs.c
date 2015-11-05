c9db53b87530090e796b801efe0be860a4b1feee
Dmitry Marakasov <amdmi3@amdmi3.ru>
2015-11-05 20:02:48 +0300

    Update copyright years

--- findmaildirs.c
+++ findmaildirs.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 Dmitry Marakasov <amdmi3@amdmi3.ru>
+ * Copyright (c) 2012-2015 Dmitry Marakasov <amdmi3@amdmi3.ru>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
